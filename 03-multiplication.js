ebd1714f2fe8da41ac922a2e2c9633c63faa78ff
keren2906 <kerenmatuli2906@gamil.com>
2025-10-23 15:20:02 +0200

    feat: name the function 'multiplication' and have it return the result

--- 03-multiplication.js
+++ 03-multiplication.js
@@ -15,8 +15,11 @@ function multiplication(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') return undefined;
   if (isNaN(a) || isNaN(b)) return undefined;
 
-  return a * b;
+  const result = a * b;
+  return Object.is(result, -0) ? 0 : result;
+
 }
+console.log("multiplication(3, 4) =", multiplication(3, 4));
 
 // Ne pas modifier la ligne ci-dessous
 module.exports = { multiplication }
