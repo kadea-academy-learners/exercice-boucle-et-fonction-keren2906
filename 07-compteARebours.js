89578a8cb0967d8c4a0e9b086e983f7af09638dc
keren2906 <kerenmatuli2906@gamil.com>
2025-10-23 16:27:31 +0200

    fix: modification of exercise 7

--- 07-compteARebours.js
+++ 07-compteARebours.js
@@ -10,10 +10,12 @@
     Placeholder : version neutre (ne log rien) pour permettre l'exécution des tests
     sans lever d'exception. L'étudiant doit remplacer la fonction par son implémentation.
   */
-  function compteARebours() {
-    // Placeholder neutre : ne fait rien
-  }
 
+  function compteARebours() {
+  for (let i = 10; i >= 1; i--) {
+    console.log(i);
+  }
+}
+compteARebours();
   // Ne pas modifier la ligne ci-dessous
   module.exports = { compteARebours }
-// Ne pas modifier la ligne ci-dessous
